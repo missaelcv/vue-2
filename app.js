456d6e6b03daeb7e7ebb4b8f3beac2441fb4d588
missaelcv <missaelcv0220@gmail.com>
2022-03-01 23:19:41 -0400

    definicion de metodo toggleShowPrices

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ new Vue ( {
     return {
       name: 'Bitcoin',
       img:  'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 10,
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      changePercent: -10,
+      
 
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
@@ -15,9 +15,16 @@ new Vue ( {
         { day: 'Jueves', value: 9000 },
         { day: 'Viernes', value: 9400 },
         { day: 'Sabado', value: 10000 },
-        { day: 'Domingo', value: 10200 },
-                       ]
+        { day: 'Domingo', value: 10200 }, ],
 
+        showPrices: false
+    }
+  },
+
+    //Deficinion de Metodo
+  methods: {
+    toggleShowPrices () {
+      this.showPrices = !this.showPrices
     }
   }
 })
