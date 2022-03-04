c74f8123b84e5242a540df0bbf9ec385eb1a88eb
missaelcv <missaelcv0220@gmail.com>
2022-03-03 23:21:01 -0400

    metodos computed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue ( {
   data () {
     return {
       name: 'Bitcoin',
+      symbol: 'BTC',
       img:  'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: -10,
       
@@ -22,6 +23,16 @@ new Vue ( {
 
         showPrices: false
     }
+  },
+
+  computed: {
+    title () { 
+      return `${this.name} - ${this.symbol}`
+    }
+  },
+
+  watch : {
+
   },
 
     //Deficinion de Metodo
